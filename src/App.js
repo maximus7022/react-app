346d9f65edfcb4d6554b2cca5b8cd7cf4816837e
maximus7022 <pash6790@gmail.com>
2023-06-16 00:16:30 +0300

    Adding workflow

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+		  <p>
+			 Nothing Else Matters
+		  </p>
         <a
           className="App-link"
           href="https://reactjs.org"
